5bdc16079b7c0b43b465b692965706e5cb4940d9
oluwafisayomi <wrld26993@gmail.com>
2023-02-27 14:36:57 +0100

    [ Init ] initialized project

---
+++ client/src/ui/Title.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="flex flex-row items-center justify-center h-12 w-full">
+            <div className="flex items-center justify-center rounded-2xl text-indigo-700 bg-indigo-100 h-10 w-10" >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0  012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                    ></path>
+                </svg>
+            </div>
+            <div className="ml-2 font-bold text-2xl">CleverTalk</div>
+        </div>
+    )
+}
+
+export default Title
